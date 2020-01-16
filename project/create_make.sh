1f50fb526bbf7072fcac6a297262fc90b2ad4f3f
Michael Gautier <michaelgautier.wordpress.com>
2020-01-16 16:37:55 -0600

    Modified make builder to output to new buildscripts/ directory.

--- project/create_make.sh
+++ project/create_make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bakefile --output=../build/build_newsreader.mk --format=gnu --verbose bakefiles/prog_newsreader.bkl
-bakefile --output=../build/build_newsdownld.mk --format=gnu --verbose bakefiles/prog_newsdownld.bkl
-bakefile --output=../build/build_rss_io_test.mk --format=gnu --verbose bakefiles/prog_rss_io_test.bkl
+bakefile --output=buildscripts/build_newsreader.mk --format=gnu --verbose bakefiles/prog_newsreader.bkl
+bakefile --output=buildscripts/build_newsdownld.mk --format=gnu --verbose bakefiles/prog_newsdownld.bkl
+bakefile --output=buildscripts/build_rss_io_test.mk --format=gnu --verbose bakefiles/prog_rss_io_test.bkl
 
