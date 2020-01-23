781ea75dfe4a6ec62d77fefcba2093364fdb9988
Michael Gautier <michaelgautier.wordpress.com>
2020-01-23 00:14:19 -0600

    Make application thread aware.
    First step in resolving improper access to the UI from a non-UI thread. Follow suggestions on the best way to make GTK thread aware and define critical sections in code that modify UI from a non-UI thread.
    
    I would get these errors near the point where the 5th RSS feed (out of 9 feeds total) was used to update the screen. The errors would not appear every time but every 3rd or 5th run of the program, the errors would appear.
    
    (newsreader): Pango-CRITICAL **: 01:12:56.777: pango_layout_set_attributes: assertion 'layout != NULL' failed
    (newsreader): Pango-CRITICAL **: 01:12:56.777: pango_layout_set_alignment: assertion 'layout != NULL' failed
    (newsreader): Pango-CRITICAL **: 01:12:56.777: pango_layout_set_ellipsize: assertion 'PANGO_IS_LAYOUT (layout)' failed
    (newsreader): Pango-CRITICAL **: 01:12:56.777: pango_layout_set_wrap: assertion 'PANGO_IS_LAYOUT (layout)' failed
    (newsreader): Pango-CRITICAL **: 01:12:56.777: pango_layout_set_single_paragraph_mode: assertion 'PANGO_IS_LAYOUT (layout)' failed
    **
    Gtk:ERROR:gtklabel.c: :gtk_label_update_layout_width: assertion failed: (priv->layout)
    Bail out! Gtk:ERROR:gtklabel.c: :gtk_label_update_layout_width: assertion failed: (priv->layout)
    ./app_run/newsreader.sh: line 3:  Aborted                 (core dumped) ../build/bin/newsreader
    
    --------------------------------------------------------------------------------
    
    I considered issues like that resolved in the following commits.
    
    26ff4a4996d5a894c608bb81dda81d41a2d31f6b
    6855edb50438482b7489e7e88825ad6753836e8e
    4dfcf47b84e5cbcf45b692c756ebf4af39f66bc9
    
    Those commits dealt with invalid pointer accesses. This new issue seems related to not setting up GTK for multi-threading. I found a trio of articles that describe the threading issues in nearly the same way. The preamble in this new version of the application.cpp module establishes the proper context for modifying the UI from a non-UI thread.
    
    --------------------------------------------------------------------------------
    
    Useful background information for modifying the UI in GTK from a non-UI thread.
    
    	GTK Threads
    
    - https://www.gnu.org/software/guile-gnome/docs/gdk/html/Threads.html
    
    	Geany GTK FAQ
    
    - https://www.geany.org/manual/gtk/gtk-faq/x482.html
    
    	Multi threaded GTK+ applications - 06/2009
    
    - http://blog.borovsak.si/2009/06/multi-threaded-gtk-applications.html

--- source/rss_ui/impl/application.cpp
+++ source/rss_ui/impl/application.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2019 Michael Gautier
+Copyright (C) 2020 Michael Gautier
 
 This source code is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
 
@@ -18,6 +18,14 @@ Author: Michael Gautier <michaelgautier.wordpress.com>
 int
 main (int argc, char** argv)
 {
+	if (! g_thread_supported()) {
+		g_thread_init (NULL);
+	}
+
+	gdk_threads_init();
+
+	gtk_init (NULL, NULL);
+
 	int status = gautier_rss_ui_app::create();
 
 	return status;
