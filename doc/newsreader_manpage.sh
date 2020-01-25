17e30c2996788f55f692a9ad7645935d0aa7ff12
Michael Gautier <michaelgautier.wordpress.com>
2020-01-24 23:32:24 -0600

    Linux manpage for newsreader.
    A manpage does not seem like a good fit but the RPM guides suggest it. I decided to write a manpage and include it in the distribution.

---
+++ doc/newsreader_manpage.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#http://www.linuxhowtos.org/System/creatingman.htm
+#https://www.cyberciti.biz/faq/linux-unix-creating-a-manpage/
+gzip --keep --synchronous --verbose --rsyncable newsreader.7
+
