746ffbe956f3e257415c7dae0a9c828146e31e9e
Michael Gautier <michaelgautier.wordpress.com>
2020-01-31 01:29:34 -0600

    Desktop application build (7.1.0) for Debian and Ubuntu Linux.
    - First packaged release that can be deployed to Ubuntu.
    - GNOME desktop integration. Style sheet works 100%, icon still needs work.
    - Deployment tested on a vanilla install of Ubuntu 19.10 (virtual machine).
    - As of this commit, the program can be installed on Ubuntu or Fedora.

--- project/package_build/build_deb.sh
+++ project/package_build/build_deb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Primary purpose is to generate a deployment package that validates the program's conformance to Linux standards.
+#This is based on the build_rpm.sh script since that script came first.
 
 #Goals
 #	1) Generate a source package that meets the requirements for Linux distributions.
@@ -8,28 +9,80 @@
 
 #All files included in the source DEB build are to be single licensed under LGPLv2+
 
-#Prepare to build archives, executables and DEBs
+#Prepare to build archives, executables and RPMs
 ## **	Delete stale build files.
-mkdir -p ~/newsreader-package/
-rm -rf ~/newsreader-package/
-mkdir -p ~/newsreader-package/
+mkdir -p ~/debbuild/
+rm -rf ~/debbuild/
+mkdir -p ~/debbuild/packaging/
 
-mkdir -p ~/newsreader-package.orig/
-rm -rf ~/newsreader-package.orig/
-mkdir -p ~/newsreader-package.orig/build/
+mkdir -p ~/debbuild_tmp/
+rm -rf ~/debbuild_tmp/
+mkdir -p ~/debbuild_tmp/
 
-cp --recursive --preserve ../project/package_build/debian/ ~/newsreader-package/
+echo "-------------------------------------------------------------"
+pwd
+ls -gGh
+#DEB Package Configuration
+## **	Establish build configuration under the debian/ build tree.
+rsync --human-readable --times --executability --preallocate --update --delay-updates --recursive ../project/package_build/debian ~/debbuild/packaging/
+cp --update --preserve ../LICENSE ~/debbuild/packaging/debian/copyright
 
-rsync --human-readable --times --executability --preallocate --update --delay-updates --recursive ../source/rss_ui ~/newsreader-package.orig/source/
-rsync --human-readable --times --executability --preallocate --update --delay-updates --recursive ../source/rss_lib ~/newsreader-package.orig/source/
+#Source Archive - Part 1
+## **	Populate archive setup directories. The goal is to add files to the setup directories pertinent to the installation.
+## **	This is the precise source tree needed to build the program.
+rsync --human-readable --times --executability --preallocate --update --delay-updates --recursive ../source/rss_ui ~/debbuild_tmp/source/
+rsync --human-readable --times --executability --preallocate --update --delay-updates --recursive ../source/rss_lib ~/debbuild_tmp/source/
 
-rsync --human-readable --times --executability --preallocate --update --delay-updates --recursive ../doc ~/newsreader-package.orig/
-rsync --human-readable --times --executability --preallocate --update --delay-updates --recursive ../desktop ~/newsreader-package.orig/
+rsync --human-readable --times --executability --preallocate --update --delay-updates --recursive ../doc ~/debbuild_tmp/
+rsync --human-readable --times --executability --preallocate --update --delay-updates --recursive ../desktop ~/debbuild_tmp/
 
+#Source Archive - Part 2a
 ## **	Transform newsreader makefile to the default file name expected by the make program.
-rsync --human-readable --times --executability --preallocate --update --delay-updates ./buildscripts/build_newsreader.mk ~/newsreader-package.orig/build/makefile
+cp --update --preserve ./buildscripts/build_newsreader.mk ~/debbuild_tmp/makefile
+cp --update --preserve ./buildscripts/build_newsreader.mk ~/debbuild/packaging/makefile
 
-cd ~/newsreader-package/
+#Source Archive - Part 3
+## **	Generate a source tarball that works with the debian/ubuntu build program of choice.
+## **	Nearly all debian / ubuntu build processes use a source archive (compressed zip or tar.gz file).
+## **	The tarball final tarball will have a name that matches the version tag.
+echo "-------------------------------------------------------------"
+cd ~/debbuild_tmp/
+
+cp --recursive --preserve * ~/debbuild/
+
+tar -czf newsreader.tar.gz *
+
+cp --preserve newsreader.tar.gz ~/debbuild/newsreader_7.1.0.orig.tar.gz
+
+pwd
+ls -gGh ~/debbuild_tmp/
+
+echo "-------------------------------------------------------------"
+cd ~/debbuild/
+pwd
+ls -gGh ~/debbuild/
+
+#Generate source DEB
+## **	A source DEB will contain instruction on how a package should be configured on a system.
+## **	However, success building a source DEB does not mean it can be transformed into an installable DEB.
+## **	Consider building an installable DEB (also called a binary DEB) to verify correct definition.
+cd ~/debbuild/packaging/
 debuild -us -uc
 
+echo "-------------------------------------------------------------"
+pwd
+ls -gGh ~/debbuild/packaging/
+
+cd ~/debbuild/
+echo "-------------------------------------------------------------"
+pwd
+ls -gGh ~/debbuild/
+
+#Generate installation DEB (binary executable)
+## **	Intent here is to test the generated source DEB while generating the binary DEB.
+#		debbuild --rebuild --nodebuginfo ~/debbuild/SRPMS/newsreader-7.1.0-1.fc31.src.rpm
+
+#Clean-up
+## **	Delete preliminary setup files
+rm -rf ~/debbuild_tmp/
 
