097fe88ae4a9b67851102c42fbe2e04e05f30296
Michael Gautier <michaelgautier.wordpress.com>
2020-03-17 21:49:50 -0500

    [Microsoft Windows] Attempt at a Microsoft Windows version using MinGW-64.

---
+++ project/buildscripts/build_newsreader_mingw.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+make --file=build_newsreader_mingw.mk CXX=x86_64-w64-mingw32-g++ CC=x86_64-w64-mingw32-gcc
+
--- project/create_make.sh
+++ project/create_make.sh
@@ -2,6 +2,7 @@
 
 bakefile --output=buildscripts/build_newsreader.mk --format=gnu --verbose bakefiles/prog_newsreader.bkl
 bakefile --output=buildscripts/build_newsreader_clang.mk --format=gnu --verbose bakefiles/prog_newsreader_clang.bkl
+bakefile --output=buildscripts/build_newsreader_mingw.mk --format=gnu --verbose bakefiles/prog_newsreader_mingw.bkl
 
 bakefile --output=buildscripts/build_newsdownld.mk --format=gnu --verbose bakefiles/prog_newsdownld.bkl
 bakefile --output=buildscripts/build_rss_io_test.mk --format=gnu --verbose bakefiles/prog_rss_io_test.bkl
